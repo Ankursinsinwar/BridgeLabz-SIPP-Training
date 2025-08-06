a9934df4f46a319b0a57040678c96cdd23d02171
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2025-08-06 23:07:55 +0530

    [Ankur Sinsinwar].Add Unchecked Exception (Runtime Exception) problems for Exception Handling

---
+++ Java_Exceptions/Unchecked_Exception/Division.java
@@ -0,0 +1,20 @@
+package Java_Exceptions.Unchecked_Exception;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter number 1: "); int num1 = sc.nextInt();
+            System.out.print("Enter number 2: "); int num2 = sc.nextInt();
+            System.out.println("Result: " + (num1 / num2));
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        }
+        sc.close();
+    }
+}
